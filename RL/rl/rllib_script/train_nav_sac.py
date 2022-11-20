524be8e2928fd22638518e0a48cecbec35c12c37
qintianhaohao <qintianhaohao@163.com>
2022-11-20 23:44:36 +0800

    创建train_nav_sac.py，使用sac算法训练

--- RL/rl/rllib_script/train_nav_sac.py
+++ RL/rl/rllib_script/train_nav_sac.py
@@ -6,14 +6,15 @@ import rl.rllib_script.agent.model.ray_model
 from blimp_env.envs import ResidualPlanarNavigateEnv
 from blimp_env.envs.script import close_simulation
 from ray import tune
-from ray.rllib.agents import ppo
+# from ray.rllib.agents import ppo
+from ray.rllib.algorithms import sac
 from ray.tune.registry import register_env
 from rl.rllib_script.util import find_nearest_power_of_two
 
 # exp setup
 ENV = ResidualPlanarNavigateEnv
-AGENT = ppo
-AGENT_NAME = "PPO"
+AGENT = sac
+AGENT_NAME = "SAC"
 exp_name_posfix = "test"
 
 env_default_config = ENV.default_config()
@@ -21,24 +22,17 @@ duration = env_default_config["duration"]
 simulation_frequency = env_default_config["simulation_frequency"]
 policy_frequency = env_default_config["policy_frequency"]
 
-days = 28
+days = 2
 one_day_ts = 24 * 3600 * policy_frequency
 TIMESTEP = int(days * one_day_ts)
-
 restore = None
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--gui", type=bool, default=True, help="Start with gazebo gui")
+parser.add_argument("--gui", type=bool, default=False, help="Start with gazebo gui")
 parser.add_argument("--num_gpus", type=bool, default=1, help="Number of gpu to use")
-parser.add_argument(
-    "--num_workers", type=int, default=1, help="Number of workers to use"
-)
-parser.add_argument(
-    "--stop_timesteps", type=int, default=TIMESTEP, help="Number of timesteps to train."
-)
-parser.add_argument(
-    "--resume", type=bool, default=False, help="resume the last experiment"
-)
+parser.add_argument("--num_workers", type=int, default=4, help="Number of workers to use")
+parser.add_argument("--stop_timesteps", type=int, default=TIMESTEP, help="Number of timesteps to train.")
+parser.add_argument("--resume", type=bool, default=False, help="resume the last experiment")
 parser.add_argument(
     "--use_lstm",
     dest="use_lstm",
@@ -60,9 +54,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(f"Running with following CLI options: {args}")
     ray.init(local_mode=False)
-
-    # env config
     register_env(env_name, env_creator)
+
     init_alt = 100
     square = [
         (40, 40, -init_alt, 3),
@@ -79,41 +72,39 @@ if __name__ == "__main__":
             "enable_wind": False,
             "enable_wind_sampling": False,
             "enable_buoyancy_sampling": False,
-            "position": (0, 0, init_alt),
         },
-        "target": {
-            "type": "MultiGoal",
-            "target_name_space": "goal_",
-            "trigger_dist": 5,
-            "wp_list": square,
-            "enable_random_goal": False,
-        },
+        # "target": {
+        #     "type": "MultiGoal",
+        #     "target_name_space": "goal_",
+        #     "trigger_dist": 5,
+        #     "wp_list": square,
+        #     "enable_random_goal": False,
+        # },
         "mixer_type": "absolute",
-        "mixer_param": (0.5, 0.01),  # alpha, beta
+        "mixer_param": (0.5, 0.5),  # alpha, beta
         "enable_residual_ctrl": True,
     }
 
-    if args.use_lstm:
-        custom_model = "bnrnn_model"
-        custom_model_config = {
-            "hidden_sizes": [64, 64],
-            "lstm_cell_size": 64,
-            "lstm_use_prev_action": True,
-            "lstm_use_prev_reward": True,
-        }
-        print("BNRNN model selected")
-    else:
-        custom_model = "bn_model"
-        custom_model_config = {
-            "actor_sizes": [64, 64],
-            "critic_sizes": [128, 128],
-        }
-        print("BN model selected")
-
-    model_config = {
-        "custom_model": custom_model,
-        "custom_model_config": custom_model_config,
-    }
+    # if args.use_lstm:
+    #     custom_model = "bnrnn_model"
+    #     custom_model_config = {
+    #         "hidden_sizes": [64, 64],
+    #         "lstm_cell_size": 64,
+    #         "lstm_use_prev_action": True,
+    #         "lstm_use_prev_reward": True,
+    #     }
+    #     print("BNRNN model selected")
+    # else:
+    #     custom_model = "bn_model"
+    #     custom_model_config = {
+    #         "actor_sizes": [64, 64],
+    #         "critic_sizes": [128, 128],
+    #     }
+    #     print("BN model selected")
+    # model_config = {
+    #     "custom_model": custom_model,
+    #     "custom_model_config": custom_model_config,
+    # }
 
     episode_ts = duration * policy_frequency / simulation_frequency
     train_batch_size = args.num_workers * 4 * episode_ts
@@ -129,23 +120,23 @@ if __name__ == "__main__":
             "num_workers": args.num_workers,  # parallelism
             "num_envs_per_worker": 1,
             "framework": "torch",
-            "model": model_config,
+            # "model": model_config,
             # == Learning ==
             "gamma": 0.999,
-            "lambda": 0.9,
-            "kl_coeff": 1.0,
+            # "lambda": 0.9,
+            # "kl_coeff": 1.0,
             "horizon": episode_ts,
             "rollout_fragment_length": episode_ts,
             "train_batch_size": train_batch_size,
-            "sgd_minibatch_size": sgd_minibatch_size,
-            "num_sgd_iter": 32,
+            # "sgd_minibatch_size": sgd_minibatch_size,
+            # "num_sgd_iter": 32,
             "lr": 1e-4,
-            "lr_schedule": [
-                [0, 1e-4],
-                [args.stop_timesteps, 5e-6],
-            ],
-            "clip_param": 0.2,
-            "vf_clip_param": 10,
+            # "lr_schedule": [
+            #     [0, 1e-4],
+            #     [args.stop_timesteps, 5e-6],
+            # ],
+            # "clip_param": 0.2,
+            # "vf_clip_param": 10,
             "grad_clip": 1.0,
             "observation_filter": "NoFilter",
             "batch_mode": "truncate_episodes",
@@ -155,6 +146,7 @@ if __name__ == "__main__":
         "timesteps_total": args.stop_timesteps,
     }
 
+    print('---------- config ----------')
     print(config)
     if env_config["simulation"]["auto_start_simulation"]:
         close_simulation()
@@ -164,7 +156,7 @@ if __name__ == "__main__":
         name=exp_name,
         config=config,
         stop=stop,
-        checkpoint_freq=1,
+        checkpoint_freq=10,
         checkpoint_at_end=True,
         reuse_actors=False,
         restore=restore,
